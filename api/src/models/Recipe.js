62ccf71fb1dcd87ce64afa5d07da0089858cc3ba
LeslieQuet <quetglaslesllie@gmail.com>
2023-05-09 13:53:46 -0300

    DB MODELS OK

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,33 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    resume: {
+      type: DataTypes.STRING, //Agregar máximos y min?
+      allowNull: false,
+    },
+    health_score: {
+      type: DataTypes.STRING, //Son números???
+      allowNull: false,
+    },
+    step_by_step: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, 
+  {
+    timestamps: false,
   });
 };
