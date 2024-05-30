ee1719aa7676450cac8bb265cb3a23eda1d1aaae
LeslieQuet <quetglasleslie@gmail.com>
2024-05-30 19:04:59 -0300

    back url

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './Redux/store';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'https://foodsback.onrender.com'
+axios.defaults.baseURL = 'https://foods-spa-back.vercel.app'
 
 ReactDOM.render(
   <Provider store={store}>
